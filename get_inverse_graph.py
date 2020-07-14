8c14078ad21c5f1afe3dbc2ff4326bcad7ef857f
shuaiwangvu <shuai.wang.vu@gmail.com>
2020-07-14 20:29:28 +0200

    small

---
+++ get_inverse_graph.py
@@ -0,0 +1,24 @@
+# this is a python script that computes the inverse of a graph from its edgelist
+
+import sys
+import csv
+
+
+def compute_inverse_graph ():
+    print (str(sys.argv))
+    filename = sys.argv[1]
+    outputfilename = filename+'-t'
+    print ('file name = ', filename)
+    with open(filename, 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=' ')
+        outputfile = open(outputfilename, 'w', newline='')
+        writer = csv.writer(outputfile, delimiter='\t')
+        for row in csv_reader:
+            left = row[0]
+            right = row[1]
+            writer.writerow([right, left])
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    compute_inverse_graph()
